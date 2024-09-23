c19b5618b301a569576b2867035dbb343ad3fa66
ArthKekl1108 <163012270+ArthKekl1108@users.noreply.github.com>
2024-09-23 11:58:46 -0300

    commitandoCodígoFinal

--- js/principal.js
+++ js/principal.js
@@ -12,5 +12,5 @@ for(var contador = 0; contador < pacientes.length; contador++ ){
     var peso = pacientes[contador].querySelector('.info-peso').textContent;
     var altura = pacientes[contador].querySelector('.info-altura').textContent;
     var imc = peso / (altura*altura);
-    pacientes[contador].querySelector('info-imc').textContent = imc;
+    pacientes[contador].querySelector('.info-imc').textContent = imc;
 }
